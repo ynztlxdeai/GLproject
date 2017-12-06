74c641b6641c5d3c0a39d2c8a210454ed572bb5a
Luoxiang <ynztlxdeai@163.com>
2017-12-06 11:05:31 +0800

    finishe Belt

---
+++ app/src/main/java/com/luoxiang/glproject/domain/Circle.java
@@ -0,0 +1,18 @@
+package com.luoxiang.glproject.domain;
+
+/**
+ * projectName: 	    GLproject
+ * packageName:	        com.luoxiang.glproject.domain
+ * className:	        Circle
+ * author:	            Luoxiang
+ * time:	            2017/12/6	10:12
+ * desc:	            三角形扇面绘制类
+ *
+ * svnVersion:	        $Rev
+ * upDateAuthor:	    Vincent
+ * upDate:	            2017/12/6
+ * upDateDesc:	        TODO
+ */
+public class Circle {
+
+}
