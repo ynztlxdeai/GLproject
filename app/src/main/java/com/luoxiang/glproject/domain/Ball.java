3b914e5354e9fd1653fdfa987968daf8d662a688
Vincent <luoxiang@megvii.com>
2018-04-18 10:33:53 +0800

    create Ball.java and vertex , fragment shader text

--- app/src/main/java/com/luoxiang/glproject/domain/Ball.java
+++ app/src/main/java/com/luoxiang/glproject/domain/Ball.java
@@ -1,5 +1,10 @@
 package com.luoxiang.glproject.domain;
 
+import android.content.Context;
+import android.opengl.GLES20;
+
+import java.nio.FloatBuffer;
+
 /**
  * projectName: 	    GLproject
  * packageName:	        com.luoxiang.glproject.domain
@@ -16,5 +21,48 @@ package com.luoxiang.glproject.domain;
 
 
 public class Ball {
+    //渲染管着色程序
+    int mProgram;
+    //总变换矩阵引用
+    int muMVPMatrixHandle;
+    //顶点位置属性引用
+    int maPositionHandle;
+    //球的半径参数引用
+    int muRHandle;
+    //顶点着色器代码
+    String mVertexShader;
+    //片元着色器代码
+    String mFragmentShader;
+    //顶点坐标数据缓冲区
+    FloatBuffer mVertexBuffer;
+    //顶点数量
+    int vCount;
+    //绕X Y Z 旋转的角度
+    float yAngle = 0;
+    float xAngle = 0;
+    float zAngle = 0;
+    //球半径
+    float r = 0.8f;
 
+
+    public Ball(Context context) {
+        initVertexData();
+        initShader(context);
+    }
+
+    //顶点着色器和片元着色器加载
+    private void initShader(Context context) {
+
+        muRHandle = GLES20.glGetUniformLocation(mProgram , "uR");
+    }
+
+    //顶点坐标初始化
+    private void initVertexData() {
+
+    }
+
+    public void drawSelf(){
+
+
+    }
 }
